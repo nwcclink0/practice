2b5f4ff8beaa9b214b4c198c30258e57cb961a3a
moby <9712579@gmail.com>
2016-09-26 21:01:34 +0800

    add epoll client

---
+++ epoll/epoll-client.c
@@ -0,0 +1,197 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <errno.h>
+
+#define MAXEVENTS 64
+#define EPOLL_RUN_TIMEOUT 1000
+
+#define SUCCESS 0
+#define ERROR 1
+
+static int set_nonblocking_socket(int socket_fd);
+static int socket_create_bind(char *address, int port)
+{
+    int s, socket_fd;
+    socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    /* struct addrinfo hints; */
+    /* struct addrinfo *rst, *rp; */
+    struct sockaddr_in sa = {0};
+    struct sockaddr_in *psa = NULL;
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port);
+    psa = &sa;
+
+    printf("address is :%s\n", address);
+    int rc = inet_pton(AF_INET, address, &psa->sin_addr.s_addr);
+    if(rc == 0) {
+        printf("inet_pton failed, invalid address\n");
+        close(socket_fd);
+        return ERROR;
+    } else if(rc < 0) {
+        printf("connect failed\n");
+        close(socket_fd);
+        return ERROR;
+    }
+
+    if(connect(socket_fd, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
+        printf("connect failed");
+        return ERROR;
+    }
+    set_nonblocking_socket(socket_fd);
+    printf("success and the socket fd is: %d\n", socket_fd);
+    return socket_fd;
+    /* memset(&hints, 0, sizeof(struct addrinfo)); */
+    /* hints.ai_family = AF_UNSPEC; */
+    /* hints.ai_socktype = SOCK_STREAM; */
+    /* hints.ai_flags = AI_ADDRCONFIG; */
+
+    /* s = getaddrinfo(address, NULL, &hints, &rst); */
+    /* if(s){ */
+    /*     printf("getaddrinfo return error: %d\n", s); */
+    /*     return ERROR; */
+    /* } */
+#if 0
+    for(rp = rst; rp != NULL; rp = rp->ai_next) {
+        socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if(socket_fd == -1) continue;
+
+        if(rp->ai_family == PF_INET) {
+            ((struct sockaddr_in *)rp->ai_addr)->sin_port = htons(port);
+        } else if(rp->ai_family == PF_INET6) {
+            ((struct sockaddr_in6 *)rp->ai_addr)->sin6_port = htons(port);
+        } else {
+            close(socket_fd);
+            continue;
+        }
+
+        /* s = bind (socket_fd, rp->ai_addr, rp->ai_addrlen); */
+        /* if(s == 0){ */
+        /*     break; */
+        /* } */
+
+        set_nonblocking_socket(socket_fd);
+        int rc = connect(socket_fd, rp->ai_addr, rp->ai_addrlen);
+        if(rc == 0 || errno == EINPROGRESS || errno == EWOULDBLOCK) {
+            if(rc < 0 && (errno == EINPROGRESS || errno == EWOULDBLOCK)) {
+                printf("error on connect, rc: %d, %s, %d", rc, __func__, __LINE__);
+            }
+            break;
+        }
+
+        close(socket_fd);
+        printf("close socket fd:%d\n", socket_fd);
+    }
+
+    if(rp == NULL) {
+        printf("could not bind\n");
+        return ERROR;
+    }
+
+
+    freeaddrinfo(rst);
+    return socket_fd;
+#endif
+}
+
+static int set_nonblocking_socket(int socket_fd)
+{
+    int flags;
+    int s;
+    flags = fcntl (socket_fd, F_GETFL, 0);
+    if(flags == -1) {
+        printf("fctl %s %d\n", __func__, __LINE__);
+        return ERROR;
+    }
+
+    flags |= O_NONBLOCK;
+    s = fcntl(socket_fd, F_SETFL, flags);
+    if( s == -1) {
+        printf("fctl %s %d\n", __func__, __LINE__);
+    }
+    return SUCCESS;
+}
+
+int handle_write_event(int fd)
+{
+    char buf[1024] = {0};
+
+    snprintf(buf, 1024, "this is pid server: %d\n", getpid());
+    if(write(fd, buf, 1024) < 0) {
+        printf("write error, %s %d\n", __func__, __LINE__);
+    }
+    return 0;
+}
+
+int handle_read_event(int fd)
+{
+    ssize_t count;
+    char buf[1024] = {0};
+
+    count = read(fd, buf, 1024);
+    if(count == -1) {
+        if(errno != EAGAIN) {
+            printf("read\n");
+        }
+    }
+
+
+    int sock = write(1, buf, count);
+    if(sock == -1) {
+        printf("write erro: %s %d\n", __func__, __LINE__);
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        fprintf (stderr, "Usage: %s [host] [port]\n", argv[0]);
+        exit (EXIT_FAILURE);
+    }
+
+    int efd;
+    efd = epoll_create(MAXEVENTS);
+    static struct epoll_event ev;
+    static struct epoll_event *events;
+
+    int client_sock;
+    client_sock = socket_create_bind(argv[1], atoi(argv[2]));
+    if(client_sock == ERROR) return 0;
+
+    ev.data.fd = client_sock;
+    /* ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLHUP | EPOLLOUT; */
+    ev.events = EPOLLOUT | EPOLLIN |EPOLLET;
+
+    events = calloc(MAXEVENTS, sizeof(ev));
+
+    int res = epoll_ctl(efd, EPOLL_CTL_ADD, client_sock, &ev);
+
+    while(1) {
+
+        int nfds = epoll_wait(efd, events, MAXEVENTS, EPOLL_RUN_TIMEOUT);
+        if(nfds < 0)
+            printf("Error in epoll wait\n");
+
+        for(int i = 0; i < nfds; i++) {
+            if(events[i].events & EPOLLIN) {
+                printf("get EPOLLIN event\n");
+                handle_read_event(events[i].data.fd);
+            } else if(events[i].events & EPOLLOUT) {
+                printf("get EPOLLOUT event\n");
+                handle_write_event(events[i].data.fd);
+            }
+        }
+    }
+
+    close(client_sock);
+    close(efd);
+}
